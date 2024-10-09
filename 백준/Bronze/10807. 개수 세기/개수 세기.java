d0d6edfe858e4ef2f71e0dd8455868e53c7c55b0
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-09 19:50:39 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -6,21 +6,14 @@ class Main {
 	    
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
+	    int cnt = 0;
 	    int n = Integer.parseInt(br.readLine());
 	    
-	    int[] arr = new int[n];
-	    
 	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    for(int i=0; i<n; i++) {
-	        arr[i] = Integer.parseInt(st.nextToken());
-	    }
-	    
 	    int m = Integer.parseInt(br.readLine());
 	    
-	    int cnt = 0;
-	    
-	    for(int x : arr) {
-	        if(x==m) cnt++;
+	    while(st.hasMoreTokens()) {
+	        if(Integer.parseInt(st.nextToken()) == m) cnt++;
 	    }
 	    
 	    System.out.print(cnt);
