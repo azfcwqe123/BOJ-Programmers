96a6ede47bf35c4f35be499c27a91a4cd762a656
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-12 17:55:19 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main (String[] args) throws IOException {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    String str = br.readLine();
+	    
+	    int[] arr = new int[26];
+	    
+	    for(int i=0; i<26; i++) {
+	        arr[i] = -1;
+	    }
+	    
+	    for(int i=0; i<str.length(); i++) {
+	        if(arr[str.charAt(i) - 'a'] == -1 ) arr[str.charAt(i) - 'a'] = i;
+	    }
+	    
+	    for(int x : arr) {
+	        System.out.print(x + " ");
+	    }
+	    
+	}
+	
+}   
+
+
