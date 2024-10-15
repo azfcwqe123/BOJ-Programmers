fcaca3ed255adab53f8cafa4d0773abb1c019022
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-15 19:14:45 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -9,9 +9,12 @@ class Main {
 	 
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    
-	    double a = 0;
-	    double b = 0;
+	    String[] A = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+	    double[] B = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
 	        
+	    double sum1 = 0;
+	    double sum2 = 0;
+	    
 	    for(int i=0; i<20; i++) {
 	        StringTokenizer st = new StringTokenizer(br.readLine());
 	        
@@ -19,39 +22,22 @@ class Main {
 	        double score = Double.parseDouble(st.nextToken());
 	        String grade = st.nextToken();
 	        
-	        if(!(grade.equals("P"))) {
-	            
-	            if(grade.equals("A+")) {
-	                a += 4.5 * score;
-	            } else if(grade.equals("A0")) {
-	                a += 4.0 * score;
-	            } else if(grade.equals("B+")) {
-	                a += 3.5 * score;
-	            } else if(grade.equals("B0")) {
-	                a += 3.0 * score;
-	            } else if(grade.equals("C+")) {
-	                a += 2.5 * score;
-	            } else if(grade.equals("C0")) {
-	                a += 2.0 * score;
-	            } else if(grade.equals("D+")) {
-	                a += 1.5 * score;
-	            } else if(grade.equals("D0")) {
-	                a += 1.0 * score;
-	            } else if(grade.equals("F")) {
-	                a += 0.0 * score;
-	            }
-	            
-	            b += score;
-
-	            }
-	            
+	        for(int j=0; j<A.length; j++) {
+	            if(A[j].equals(grade)) {
+	                sum1 += B[j] * score;
+	                sum2 += score;
+	                break;
+	            }   
 	        }
 	        
-	        System.out.print(String.format("%6f",(a/b)));
-	        
 	    }
-	    
+	        
+	   System.out.print(String.format("%6f", (sum1 / sum2)));
+	        
+	        
 	}
+	    
+}
 	
 
 
