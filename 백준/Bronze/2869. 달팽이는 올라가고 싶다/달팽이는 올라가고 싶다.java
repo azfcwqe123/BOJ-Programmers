833a10c11c99cad529196692dc06257dbad8d82f
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-20 15:28:46 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    
+	    int A = Integer.parseInt(st.nextToken());
+	    int B = Integer.parseInt(st.nextToken());
+	    int V = Integer.parseInt(st.nextToken());
+	    
+	    int ans = (V-A)/(A-B);
+	    
+	    if((V-A)%(B-A) == 0) {
+	        ans++;
+	    } else ans+=2;
+	    
+	    System.out.print(ans);
+ 	    
+	}
+	    
+}
+	
+
+
