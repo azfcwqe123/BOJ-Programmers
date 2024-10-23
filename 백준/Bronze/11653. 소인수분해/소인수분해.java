44c5481d5c8ab839aa3c6f0dab446a728773c4e1
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-23 14:43:15 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int n = Integer.parseInt(br.readLine());
+	    
+	    StringBuilder sb = new StringBuilder();
+	    
+	    for(int i=2; i<=n; i++) {
+	 
+	        while(n % i == 0) {
+	            sb.append(i).append("\n");
+	            
+	            n /= i;
+	        }       
+	       
+	    }
+	    
+	    
+	    
+	    System.out.print(sb);
+	    
+	}
+}
+
+
+
