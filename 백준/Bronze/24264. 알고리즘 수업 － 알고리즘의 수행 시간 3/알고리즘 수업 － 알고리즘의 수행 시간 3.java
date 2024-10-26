d3b7be41bd2a0aeb7d0595d24bff7ae397f90942
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-26 12:48:42 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int n = Integer.parseInt(br.readLine());
+	    System.out.println((long) n*n);
+	    System.out.print(2);
+	    
+	}
+}
+
+
+
