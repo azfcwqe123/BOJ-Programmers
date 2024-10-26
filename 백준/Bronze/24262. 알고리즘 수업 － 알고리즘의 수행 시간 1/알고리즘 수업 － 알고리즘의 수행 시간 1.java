1c8e6c9395613ffcad85390923bebb360b5675ae
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-10-26 12:45:04 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+	    
+	    System.out.println(1);
+	    System.out.print(0);
+	    
+	}
+}
+
+
+
