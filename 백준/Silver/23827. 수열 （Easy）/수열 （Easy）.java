3daeb05581e07b3c04059a28141c4b52896adff4
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-04 20:22:50 +0900

    [Silver IV] Title: 수열 (Easy), Time: 380 ms, Memory: 59984 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+	    
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	    
+	    int n = Integer.parseInt(br.readLine());
+	    
+	    long mod = 1_000_000_007;
+	    
+	    long[] arr = new long[n+1];
+	    long[] sum = new long[n+1];
+	    
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    for(int i=1; i<=n; i++) arr[i] = Long.parseLong(st.nextToken());
+	    
+	    for(int i=1; i<=n; i++) sum[i] = sum[i-1] + arr[i];
+	    
+	    long ans = 0;
+	    for(int i=1; i<n; i++) {
+	        ans += (sum[n] - sum[i]) * arr[i];    
+	        
+	        ans %= mod;
+	    }
+	    
+	    System.out.print(ans);
+	    
+    }
+
+}
