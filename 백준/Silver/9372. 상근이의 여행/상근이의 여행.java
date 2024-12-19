12943105680a17a596e8c889eb49b2e9b6d98706
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-19 18:09:13 +0900

    [Silver IV] Title: 상근이의 여행, Time: 424 ms, Memory: 55148 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -6,9 +6,9 @@ class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
     
-    public static int[][] graph;
+    public static ArrayList<ArrayList<Integer>> graph;
     public static int[] ch;
-    public static int N, M, L;
+    public static int N, M, ans;
     
     public static void main(String[] args) throws IOException {
         
@@ -21,7 +21,9 @@ class Main {
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             
-            graph = new int[N+1][N+1];
+            graph = new ArrayList<>();
+            for(int i=0; i<=N; i++) graph.add(new ArrayList<>());
+            
             ch = new int[N+1];
             
             for(int i=0; i<M; i++) {
@@ -30,33 +32,35 @@ class Main {
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());    
             
-                graph[a][b] = 1;
-                graph[b][a] = 1;
+                graph.get(a).add(b);
+                graph.get(b).add(a);
             }    
+            
+            ans = 0;
             
             ch[1] = 1;
             
             BFS();
             
-            System.out.println(L - 1);
+            System.out.println(ans);
+
         }
         
         
     }
     
     public static void BFS() {
+        
         Queue<Integer> Q = new LinkedList<>();
         Q.offer(1);
         
-        L = 0;
-        
         while(!Q.isEmpty()) {
             int cur = Q.poll();
-            L++;
-            for(int i=1; i<=N; i++) {
-                if(graph[cur][i] == 1 && ch[i] == 0) {
-                    ch[i] = 1;
-                    Q.add(i);
+            for(int x : graph.get(cur)) {
+                if(ch[x] == 0) {
+                    Q.offer(x);
+                    ch[x] = 1;
+                    ans++;
                 }
             }
         }
