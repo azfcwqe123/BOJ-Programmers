27e55e12953a3ec88bb5eda1ecf6c59b8711e5e1
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-19 18:45:32 +0900

    [Silver IV] Title: 상근이의 여행, Time: 328 ms, Memory: 45816 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -6,10 +6,6 @@ class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
     
-    public static ArrayList<ArrayList<Integer>> graph;
-    public static int[] ch;
-    public static int N, M, ans;
-    
     public static void main(String[] args) throws IOException {
         
         int T = Integer.parseInt(br.readLine());
@@ -18,53 +14,19 @@ class Main {
             
             st = new StringTokenizer(br.readLine());
             
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            
-            graph = new ArrayList<>();
-            for(int i=0; i<=N; i++) graph.add(new ArrayList<>());
-            
-            ch = new int[N+1];
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             
             for(int i=0; i<M; i++) {
                 st = new StringTokenizer(br.readLine());
                 
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());    
-            
-                graph.get(a).add(b);
-                graph.get(b).add(a);
             }    
             
-            ans = 0;
-            
-            ch[1] = 1;
-            
-            BFS();
-            
-            System.out.println(ans);
+            System.out.println(N-1);
+        }
 
-        }
-        
-        
-    }
-    
-    public static void BFS() {
-        
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(1);
-        
-        while(!Q.isEmpty()) {
-            int cur = Q.poll();
-            for(int x : graph.get(cur)) {
-                if(ch[x] == 0) {
-                    Q.offer(x);
-                    ch[x] = 1;
-                    ans++;
-                }
-            }
-        }
-        
     }
     
 }
