73ce57f5f24133e6ec8b93daa5cb6c4ab409e896
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-23 17:13:56 +0900

    [Gold V] Title: 문자 해독, Time: 344 ms, Memory: 26820 KB -BaekjoonHub

--- 백준/Gold/1593. 문자 해독/문자 해독.java
+++ 백준/Gold/1593. 문자 해독/문자 해독.java
@@ -5,6 +5,8 @@ class Main {
     
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
+    public static int[] ch1;
+    public static int[] ch2;
     public static void main(String[] args) throws IOException {
         
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,37 +14,43 @@ class Main {
         int w = Integer.parseInt(st.nextToken());
         int g = Integer.parseInt(st.nextToken());
         
-        HashMap<Character, Integer> mapA = new HashMap<>();
         String W = br.readLine();
-        for(char x : W.toCharArray()) mapA.put(x, mapA.getOrDefault(x, 0) + 1);
+        String G = br.readLine();
         
-        String str = br.readLine();
-        HashMap<Character, Integer> mapB = new HashMap<>();
+        ch1 = new int[52];
+        ch2 = new int[52];
         
+        for(char x : W.toCharArray()) checkPlus(x, ch1);
+
         int ans = 0;
         
         for(int i=0; i<g; i++) {
-            char ch = str.charAt(i);
+            char tmp = G.charAt(i);
             
-            mapB.put(ch, mapB.getOrDefault(ch, 0) + 1);
+            checkPlus(tmp, ch2);
             
             if(i == w-1) {
-                if(mapA.equals(mapB)) ans++;    
+                if(Arrays.equals(ch1, ch2)) ans++;
             }
             
-            if(i >= w) {
-                char tmp = str.charAt(i-w);
-                
-                if(mapB.get(tmp) > 1) {
-                    mapB.put(tmp, mapB.get(tmp) - 1);
-                } else mapB.remove(tmp);
-                
-                if(mapA.equals(mapB)) ans++;    
+            else if(i >= w) {
+                checkMinus(G.charAt(i-w), ch2);
+                if(Arrays.equals(ch1, ch2)) ans++;
             }
         }
         
         System.out.print(ans);
         
+    }
+    
+    private static void checkPlus(char ch, int[] arr) {
+        if(Character.isUpperCase(ch)) arr[ch-39]++;
+        else arr[ch - 97]++;
+    }
+    
+    private static void checkMinus(char ch, int[] arr) {
+        if(Character.isUpperCase(ch)) arr[ch-39]--;
+        else arr[ch - 97]--;
     }
     
 }
