fc06177a4dec45424d591a3dd2ab6fe478c8c911
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-23 11:37:37 +0900

    [Silver V] Title: 폴리오미노, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -10,10 +10,47 @@ class Main {
         
         String str = br.readLine();
         
-        str = str.replaceAll("(" + "XXXX" + ")", "AAAA").replaceAll("(" + "XX" + ")", "BB");;
+        int x = 0;
         
-        if(str.contains("X")) System.out.print(-1);
-        else System.out.print(str);
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<str.length(); i++) {
+            
+            if(str.charAt(i) == '.') {
+                sb.append(".");
+                x = 0;
+            }
+            
+            if(str.charAt(i) == 'X') {
+                x++;
+                    
+                if(x == 2) {
+                    if(i+1 < str.length() && str.charAt(i+1) == '.') {
+                        sb.append("BB");
+                        x = 0;
+                    }
+                    
+                    else if(i == str.length() - 1) {
+                        sb.append("BB");
+                        x = 0;
+                    }
+                    
+                }    
+                    
+                if(x == 4) {
+                    sb.append("AAAA");
+                    x = 0;
+                }
+                
+            }
+            
+        }
+        
+        String ans = sb.toString();
+        
+        if(ans.length() != str.length()) System.out.print(-1);
+        else System.out.print(ans);
+        
     }
     
 }
