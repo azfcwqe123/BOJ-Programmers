88fb6f1bf9928d49c828ba1928fca66754b9a6a5
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-27 21:27:49 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    
+    public static void main(String[] args) throws IOException {
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        System.out.print(dp[n]);
+        
+    }
+    
+}
+
