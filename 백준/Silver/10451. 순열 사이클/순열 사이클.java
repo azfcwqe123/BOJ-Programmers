9a880d35380a223c1b6a51817af600b697e70c1a
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2024-12-30 17:37:52 +0900

    [Silver III] Title: 순열 사이클, Time: 700 ms, Memory: 114564 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    public static ArrayList<ArrayList<Integer>> graph;
+    public static boolean[] visited;
+    public static int n, m;
+    public static void main(String[] args) throws IOException {
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        while(T-- > 0) {
+            
+            int n = Integer.parseInt(br.readLine());
+            graph = new ArrayList<>();
+            visited = new boolean[n+1];
+            int ans = 0;
+            
+            for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+            
+            
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<=n; i++) {
+                int k = Integer.parseInt(st.nextToken());
+                graph.get(i).add(k);
+            }
+            
+            
+            for(int i=1; i<=n; i++) {
+                if(!visited[i]) {
+                    DFS(i);
+                    ans++;
+                }
+            }
+            
+            System.out.println(ans);
+            graph.clear();
+            Arrays.fill(visited, false);
+        }
+        
+    }
+    
+    public static void DFS(int v) {
+        
+        visited[v] = true;
+        
+        for(int nv : graph.get(v)) {
+            if(!visited[nv]) {
+                visited[nv] = true;
+                DFS(nv);
+            }
+        }
+    }
+    
+}
+
+
+
