6358fd63cda53963a0ab2c4bf69d0d53a885c8f7
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-01 12:33:58 +0900

    [Bronze V] Title: A+B - 2, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.print(a+b);
+        
+    }
+    
+}
+
