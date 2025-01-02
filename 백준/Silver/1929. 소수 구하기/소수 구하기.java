bfb3f05b395d80e5fe9543242cfad1f105218a3f
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-02 18:11:27 +0900

    [Silver III] Title: 소수 구하기, Time: 616 ms, Memory: 29532 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static boolean[] isPrime;
+    public static int m;
+    public static void main(String[] args) throws IOException {
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        isPrime = new boolean[m + 1];
+        Arrays.fill(isPrime, true);
+        
+        Check();
+        
+        for(int i=n; i<=m; i++) {
+            if(isPrime[i]) System.out.println(i);
+        }
+        
+    }
+    
+    public static void Check() {
+        
+        isPrime[0] = isPrime[1] = false;
+        
+        for(int i=2; i<=Math.sqrt(m); i++) {
+            if(!isPrime[i]) continue;
+            for(int j=i*i; j<=m; j+=i) {
+                isPrime[j] = false;
+            }
+        }
+        
+    }
+    
+}
+
