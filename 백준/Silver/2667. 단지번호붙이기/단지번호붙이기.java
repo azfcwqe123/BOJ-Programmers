0e885e9e30050bbb6b61d15c3336d8f662b1396c
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-15 18:05:08 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static class Point {
+        int x;
+        int y;
+        
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    public static ArrayList<Integer> list = new ArrayList<>();
+    
+    public static int[][] map;
+    public static int[] dx = {0, -1, 0, 1};
+    public static int[] dy = {1, 0, -1, 0};
+    public static int n;
+    
+    public static void main(String[] args) throws IOException {
+        
+        n = Integer.parseInt(br.readLine());
+        
+        map = new int[n][n];
+        
+        for(int i=0; i<n; i++) {
+            String str = br.readLine();
+            for(int j=0; j<n; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(map[i][j] == 1) {
+                    list.add(BFS(i, j));
+                    ans++;
+                }
+            }
+        }
+        
+        Collections.sort(list);
+        sb.append(ans).append("\n");
+        
+        for(int x : list) sb.append(x).append("\n");
+        
+        System.out.print(sb);
+        
+    }
+    
+    public static int BFS(int x, int y) {
+        Queue<Point> Q = new LinkedList<>();
+        Q.offer(new Point(x, y));
+        map[x][y] = 0;
+        
+        int area = 1;
+        
+        while(!Q.isEmpty()) {
+            Point cur = Q.poll();
+            
+            for(int i=0; i<4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                
+                if(rangeCheck(nx, ny) && map[nx][ny] == 1) {
+                    map[nx][ny] = 0;
+                    area++;
+                    Q.offer(new Point(nx, ny));
+                }
+            }
+        }
+        
+        return area;
+        
+    }
+    
+    private static boolean rangeCheck(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < n;
+    }
+    
+}
+
