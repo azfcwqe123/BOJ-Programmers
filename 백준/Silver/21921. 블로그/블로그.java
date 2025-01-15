5d7e18e3b407da608ac2e6b19a2532c529fced0f
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-15 23:02:33 +0900

    [Silver III] Title: 블로그, Time: 328 ms, Memory: 36072 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n+1];
+        int[] sum = new int[n+1];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum[i] = sum[i-1] + arr[i];
+        }
+        
+        int max = 0, cnt = 0;
+        
+        for(int i=x; i<=n; i++) max = Math.max(max, sum[i] - sum[i-x]);
+        
+        for(int i=x; i<=n; i++) {
+            int tmp = sum[i] - sum[i-x];
+            
+            if(tmp == max) cnt++;
+            max = Math.max(max, tmp);
+        }
+        
+        
+        if(max == 0) System.out.print("SAD");
+        else {
+            System.out.println(max);
+            System.out.println(cnt);    
+        }
+        
+    }
+    
+}
+
