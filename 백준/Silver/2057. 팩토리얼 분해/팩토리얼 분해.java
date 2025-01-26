8582907ab529bad671554bf2422463473b1904d5
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-26 14:46:21 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 132 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.java
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.java
@@ -9,7 +9,7 @@ class Main {
     public static void main(String[] args) throws IOException {
         
         long n = Long.parseLong(br.readLine());
-        long[] arr = new long[21];
+        long[] arr = new long[20];
         
         if(n == 0) {
             System.out.print("NO");
@@ -18,9 +18,9 @@ class Main {
         
         arr[0] = 1L;
         
-        for(int i=1; i<=20; i++) arr[i] = arr[i-1] * i;
+        for(int i=1; i<=19; i++) arr[i] = arr[i-1] * i;
         
-        for(int i=20; i>=0; i--) {
+        for(int i=19; i>=0; i--) {
             if(n >= arr[i]) n -= arr[i];
             
             if(n == 0) {
