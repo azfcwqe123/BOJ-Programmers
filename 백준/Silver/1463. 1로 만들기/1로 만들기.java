a985e750aaa418496859b5159a488f2d28147068
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-01-30 15:14:49 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18080 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[n+1];
+        
+        dp[0] = dp[1] = 0;
+        
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + 1;
+            
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        
+        System.out.print(dp[n]);
+        
+    }
+    
+}
+
+
