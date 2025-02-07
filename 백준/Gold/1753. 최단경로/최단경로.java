d5e08523a6a69535986bc0da6b1c53940f86b340
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-02-07 15:09:19 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 109844 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    static int[] dist;
+    static int INF = 20000 * 10 + 1;
+    
+    static class Node implements Comparable<Node> {
+        int v;
+        int cost;
+        
+        Node(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+        
+        public int compareTo(Node ob) {
+            return this.cost - ob.cost;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int start = Integer.parseInt(br.readLine());
+        
+        dist = new int[n+1];
+        Arrays.fill(dist, INF);
+        
+        for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+        
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            
+            graph.get(s).add(new Node(e, cost));
+        }
+        
+        dijkst(start);
+        
+        for(int i=1; i<=n; i++) {
+            if(dist[i] == INF) sb.append("INF");
+            else sb.append(dist[i]);
+            
+            sb.append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+    
+    static void dijkst(int start) {
+        PriorityQueue<Node> pQ = new PriorityQueue<>();
+        pQ.offer(new Node(start, 0));
+        dist[start] = 0;
+        
+        while(!pQ.isEmpty()) {
+            Node cur = pQ.poll();
+            
+            for(Node next : graph.get(cur.v)) {
+                if(dist[next.v] > dist[cur.v] + next.cost) {
+                    dist[next.v] = dist[cur.v] + next.cost;
+                    pQ.offer(new Node(next.v, dist[next.v]));
+                }
+            }
+        }
+    }
+}
+
