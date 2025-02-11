67e8d86e224983a97cb63018653f8b5197a901f0
azfcwqe123 <121239031+azfcwqe123@users.noreply.github.com>
2025-02-11 21:07:01 +0900

    [Silver V] Title: 단어 정렬, Time: 592 ms, Memory: 3988 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int compare(string a, string b) {
+    if(a.length() == b.length()) return a < b;
+    else return a.length() < b.length();
+}
+
+int main() {
+    string str[20001];
+    
+    int n;
+    cin >> n;
+    
+    for(int i=0; i<n; i++) {
+        cin >> str[i];
+    }
+    
+    sort(str, str + n, compare);
+    
+    cout << str[0] << endl;
+    for(int i=1; i<n; i++) {
+        if(str[i] == str[i-1]) continue;
+        else cout << str[i] << endl;
+    }
+    
+}
